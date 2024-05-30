7cfbcf32219ec9a2edba4d6db723b075e8b3e2dc
Matheus Castro <----->
2024-05-30 15:16:36 -0300

    fix docs for c

--- docs/conf.py
+++ docs/conf.py
@@ -25,20 +25,6 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 c_autodoc_roots = ['../meafs_code/scripts/']
 
-os.system("clang --version")
-os.system("python3 --version")
-os.system("pip list | grep libclang")
-import clang
-print(os.path.abspath(clang.__file__), "aaaaaa")
-os.system("ls /home/docs/checkouts/readthedocs.org/user_builds/meafs/envs/latest/lib/")
-os.system("ls /home/docs/checkouts/readthedocs.org/user_builds/meafs/envs/latest/lib/python3.12/")
-os.system("ls /home/docs/checkouts/readthedocs.org/user_builds/meafs/envs/latest/lib/python3.12/site-packages/")
-os.system("ls /home/docs/checkouts/readthedocs.org/user_builds/meafs/envs/latest/lib/python3.12/site-packages/clang/")
-os.system("ls /home/docs/checkouts/readthedocs.org/user_builds/meafs/envs/latest/lib/python3.12/site-packages/clang/")
-os.system("ls /home/docs/checkouts/readthedocs.org/user_builds/meafs/envs/latest/lib/python3.12/site-packages/clang/native/")
-from clang.cindex import Config
-Config.set_library_file("/home/docs/checkouts/readthedocs.org/user_builds/meafs/envs/latest/lib/python3.12/site-packages/clang/native/libclang.so")
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
@@ -47,3 +33,42 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_logo = '../meafs_code/images/Meafs_Logo.png'
 html_theme_options = {'logo_only': True}
+
+# Bug Fix for Clang bindins
+def capture_command_output(command):
+    stream = os.popen(command)
+    output = stream.read().strip()
+    return output
+
+clangver = capture_command_output("clang --version")
+clangver = clangver.replace("\n", " ")
+clangver = clangver.split(" ")
+clangver = clangver[clangver.index("version") + 1]
+clangver = clangver.split(".")[0]
+
+libclang_path = "/usr/lib/"
+candidates = []
+for i in os.listdir(libclang_path):
+    if "llvm" in i:
+        partial_path = i + "/lib/"
+        for j in os.listdir(libclang_path + partial_path):
+            if "libclang" in j and ".so" in j and "cpp" not in j:
+                candidates.append(partial_path + j)
+    elif "libclang" in i and ".so" in i and "cpp" not in i:
+        candidates.append(i)
+for i in candidates:
+    version_test = i.split("/")
+    if clangver in version_test[-1]:
+        libclang_path += i
+        break
+
+ossystem = capture_command_output("cat /etc/os-release | awk '$0 ~ /^NAME/' | awk '{split($0,a,\"=\"); print a[2]}' |"
+                                  " awk '{a=substr($0,2,length($0)-2); print a}'")
+
+from clang.cindex import Config
+
+if ossystem == "Ubuntu":
+    Config.set_library_file("/home/docs/checkouts/readthedocs.org/user_builds/meafs/envs/latest/lib/"
+                            "python3.12/site-packages/clang/native/libclang.so")
+else:
+    Config.set_library_file(libclang_path)
